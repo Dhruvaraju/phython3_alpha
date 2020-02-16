09b7ae2ee51aaec32525e51611cf976220f97a3e
dhruvaraju <49dhruva@gmail.com>
2020-02-16 11:29:23 +0530

    Version alpha

---
+++ Main.py
@@ -0,0 +1,25 @@
+print ('Console is live now');
+# To take input from user input function is used
+#var1 = input('Enter your user name: ');
+#print ('Welcome '+var1);
+# String operations
+# String Assignments for multiple line we use triple quote
+myvar1 = "String assignment with double quotes"
+myvar2 = "String assignment with single quotes"
+myvar3 ='''String
+assignment
+with
+triple
+quote'''
+myvar4 = '''String assignment \
+    with triple quotes \
+    and escape sequence'''
+#print (myvar1)
+#print (myvar2)
+#print (myvar2)
+#print (myvar4)
+# len function is used to get the length of the string
+var5 = len(myvar1)
+#str function is used to extract a string from integer
+print ('Length of string is: '+ str(var5))
+
